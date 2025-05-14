235dce8e4c9d60e0bbe54e1a1cc55ce3b7d8c9c5
Kumar <abhishek.br.kumar@capgemini.com>
2025-05-14 10:14:55 +0530

    removed commented code from DateFormatter file

--- src/utils/DateFormatter.js
+++ src/utils/DateFormatter.js
@@ -1,8 +1,6 @@
 export const formatMonth = (date) => {
   const month = date.split("-")[1];
   return month;
-  // const date = new Date(year, month - 1);
-  // return date.toLocaleString("default", { month: "long", year: "numeric" });
 };
 
 export const formatYear= (date) =>{
